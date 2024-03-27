2d50b40ff26999e5438c57e36471fad857e36c39
santirasgado <165192962+santirasgado@users.noreply.github.com>
2024-03-27 12:43:11 -0600

    Update IPadress_finder_example.py

--- IPadress_finder_example.py
+++ IPadress_finder_example.py
@@ -3,38 +3,41 @@ import folium
 import requests
 import os
 
+def fetch_location(ip, api_key):
+    try:
+        response = requests.get(f'https://ipinfo.io/{ip}/json?token={api_key}')
+        if response.status_code == 200:
+            data = response.json()
+            location = data.get('loc', None)
+            if location:
+                lat, lon = [float(coord) for coord in location.split(',')]
+                return lat, lon, data.get('city', 'Unknown city'), data.get('country', 'Unknown country')
+    except Exception as e:
+        st.error(f'Error al procesar la IP {ip}: {e}')
+    return None, None, None, None
+
 def main():
     st.title("Mapa de Ubicación de IPs")
 
-    # Ejemplo de direcciones IP para mostrar cómo ingresarlas
-    example_ips = '189.241.203.74, 3.80.247.134'
-    ip_addresses = st.text_area("Ingresa las direcciones IP, separadas por comas", example_ips)
+    # Solicita al usuario que ingrese direcciones IP
+    ip_addresses = st.text_area("Ingresa las direcciones IP, separadas por comas")
     ips = [ip.strip() for ip in ip_addresses.split(',') if ip.strip()]
 
-    # Usa una variable de entorno para la clave API
-    api_key = os.getenv('51d0481f274cac')
+    api_key = os.getenv('51d0481f274cac')  # Asegúrate de tener esta variable de entorno configurada
 
     # Crea un mapa base
-    mapa = folium.Map(location=[0, 0], zoom_start=2)
+    mapa = folium.Map(location=[20, 0], zoom_start=2)
 
     for ip in ips:
-        try:
-            response = requests.get(f'https://ipinfo.io/{ip}/json?token={api_key}')
-            if response.status_code == 200:
-                data = response.json()
-                if 'loc' in data:
-                    lat, lon = map(float, data['loc'].split(','))
-                    folium.Marker([lat, lon], popup=f'{ip} - {data.get("city", "N/A")}, {data.get("country", "N/A")}').add_to(mapa)
-                else:
-                    st.error(f'No se pudo obtener la ubicación para {ip}')
-        except Exception as e:
-            st.error(f'Error al procesar la IP {ip}: {e}')
+        lat, lon, city, country = fetch_location(ip, api_key)
+        if lat and lon:
+            folium.Marker([lat, lon], popup=f'{ip} - {city}, {country}').add_to(mapa)
 
     # Renderizar el mapa en la aplicación Streamlit
-    st_folium(mapa, width=725, height=500)
+    st_data = st_folium(mapa)
 
-# Función para mostrar el mapa en Streamlit
 def st_folium(m, width=700, height=500):
+    """Función para mostrar el mapa en Streamlit"""
     m.save('map.html')
     return st.components.v1.html(open('map.html', 'r').read(), width=width, height=height)
 
